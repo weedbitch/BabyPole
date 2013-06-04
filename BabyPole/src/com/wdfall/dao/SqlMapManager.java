a847191815149cb71a3b50782e6c72cba801f371
weedbitch <weedbitch@wdfall.com>
2013-06-04 10:34:05 +0900

    Complete Default Project Configuration

--- BabyPole/src/com/wdfall/dao/SqlMapManager.java
+++ BabyPole/src/com/wdfall/dao/SqlMapManager.java
@@ -13,36 +13,18 @@ import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 @SuppressWarnings("unchecked")
 public class SqlMapManager {
 	
-	private static String SQL_MAP_DB_TYPE_ORACLE = "SQL_MAP_ORACLE";
 	private static String SQL_MAP_DB_TYPE_MYSQL = "SQL_MAP_MYSQL";
 	
-	public static String SQL_MAP_DB_TYPE = SQL_MAP_DB_TYPE_ORACLE;
+	public static String SQL_MAP_DB_TYPE = SQL_MAP_DB_TYPE_MYSQL;
 
 	
 	private static Map sqlmapContainer ; 
 
 	static {
 		
-		String resource = "oracle-sqlmap-config.xml";
-		
-		try {
-			sqlmapContainer = new HashMap();
-			SQL_MAP_DB_TYPE = ResourceBundle.getBundle("sapphire_db").getString("db.type");
-			if( SQL_MAP_DB_TYPE.equals( SQL_MAP_DB_TYPE_MYSQL ) ) {
-				resource = "mysql/mysql-sqlmap-config.xml";
-			} else {
-				resource = "oracle-sqlmap-config.xml";
-			}
-			System.out.println("\n\n\n############# System Uses [[" + SQL_MAP_DB_TYPE + "]] ###############\n\n\n");
-			
-		} catch (Exception e) {
-			System.out.println("\n\n\n----------------------------- Info : Cannot Find sapphire.db.type properties!! -----------------------------" +
-					"\n############# System uses Default Oracle Database ###############\n\n\n");
-		} finally {
-			registerSQLMapClient(resource);
-		}
-		
-		
+		String resource = "mysql-sqlmap-config.xml";
+		sqlmapContainer = new HashMap();
+		registerSQLMapClient(resource);
 	}
 	
 	public static void registerSQLMapClient(String resource) {
